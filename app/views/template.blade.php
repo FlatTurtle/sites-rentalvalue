5fbad70f47fa65d940e5abc6bf9388fee63ca80c
Jens Segers <segers.jens@gmail.com>
2014-03-08 13:55:46 +0100

    Tweaking css

--- app/views/template.blade.php
+++ app/views/template.blade.php
@@ -3,23 +3,5 @@
 @section('style')
     @parent
 
-    <style>
-    body, nav {
-    	background-color: #000;
-    	color: #FFF;
-    }
-
-    nav a, .block h2 {
-    	color: #FFF;
-    }
-
-    h1 {
-    	color: #D3BC8D;
-    }
-
-    #content .block, #content+#reservations {
-    	background-image: none;
-    }
-    </style>
-
+    <link href="{{ URL::asset('css/custom.css') }}" rel="stylesheet">
 @stop
